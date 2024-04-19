20f2ceb5e02531f366be0e8943df9def5cfe1243
PaulNusser <126136168+PaulNusser@users.noreply.github.com>
2024-04-18 18:43:24 -0700

    Update Game.py

--- Game.py
+++ Game.py
@@ -1,17 +1,29 @@
 import random
 
 class Game:
-    wordSet = set()
+    wordList = []
     filePath = ""
     secretWord = ""
-
+    wordFound = False
+    
     def fillSet(filePath):
-
-      
-    def setHiddenWord(wordSet):
-        return random.choice(tuple(wordSet))
+        wordSet = set()
+        return wordSet
+    
+    def setToList(wordSet):
+        return list(wordSet)
+    
+    def setHiddenWord(wordList):
+        random_index = random.randint(0, (wordList.length()) - 1)
+        return wordList[random_index]
     
     def playGame():
         filePath = input("enter the filePath of the list of all words: ")
         wordSet = Game.fillSet(filePath)
-        
+        wordList = Game.setToList(wordSet)
+        secretWord = Game.setHiddenWord(wordList)
+
+        while (wordFound == False) {
+            
+        }
+
