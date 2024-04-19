32d31afb79b25a7e29d1842b677c24085628db3b
katherine-nemits <126533815+katherine-nemits@users.noreply.github.com>
2024-04-18 18:36:59 -0700

    Update WordleSolver.py

--- WordleSolver.py
+++ WordleSolver.py
@@ -1,16 +1,5 @@
 import Math
 class WordleSolver:
-  def entropy(wordList):
-    #1. Calculate information: log base 2 (1/p(x))
-    #2. Calculate probability
-    #3. Calculate entropy: E[Information] = Σp(x) * information = Entropy of word
-    #4. Get top 10 entropies
-    
-    def entropy(wordList):
-      #calculate information
-      entropyList = {}
 
-    def actualEntropy(guessStats):
-      #take in guess stats, and calculate entropy based on outcomes of the guess
         
       
