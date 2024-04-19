99b313991858807819fe4179ee6e5a213e6c3865
katherine-nemits <126533815+katherine-nemits@users.noreply.github.com>
2024-04-18 18:11:18 -0700

    Update WordleSolver.py

--- WordleSolver.py
+++ WordleSolver.py
@@ -1,1 +1,8 @@
-
+class WordleSolver:
+  def entropy(wordList):
+    #1. Calculate information: log base 2 (1/p(x))
+    #2. Calculate probability
+    #3. Calculate entropy: E[Information] = Σp(x) * log base 2 (1/p(x)) = Entropy of word
+    #4. Get top 10 entropies
+    
+    
