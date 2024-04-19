27ddd031ce70baa9a9129c629dd6666e17315863
JacksonMcCoy <126533753+JacksonMcCoy@users.noreply.github.com>
2024-04-18 17:28:56 -0700

    Update entropy.py

--- entropy.py
+++ entropy.py
@@ -1,6 +1,14 @@
 class entropy
+  def getPossibleWords(wordList, char, i):
+    possibleWords = []
+    for string in wordList:
+        if entropy.isCharInString(char,string): # idk if this line is neccesary
+          if entropy.isCharInPos(char, i, string)
+            possibleWords.append(string)
+    return possibleWords
+    
   def isCharInString(char,string):
     return char in string
 
-  def isCharInPos(string, char, i):
+  def isCharInPos(char, i, string):
     return string[i] == char and i < len(String)
