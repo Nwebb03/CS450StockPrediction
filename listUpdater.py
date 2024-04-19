4a07ff287ec79a1ac217618e0c9bc443f9e2245a
JacksonMcCoy <126533753+JacksonMcCoy@users.noreply.github.com>
2024-04-18 18:36:07 -0700

    Update listUpdater.py

--- listUpdater.py
+++ listUpdater.py
@@ -1,26 +1,33 @@
-class ListUpdater:
+class ListUpdater: # use the methods in this class to update the list of possible solutions ONLY, don't call on anything else
+                   # gameState = 2d tuple containing word_list green_list, index_list, yellow_list, grey_list
     @staticmethod
-    def delete_grey_words(word_list, grey_list):
-        for word in word_list.copy():
-            for letter in grey_list:
+    def updateValidWordsList():
+        delete_grey_words()
+        keep_yellow_words()
+        keep_grey_words()
+    
+    @staticmethod
+    def delete_grey_words(): # takes the list of currently valid words and removes all words with grey letters from it
+        for word in gameState.word_list.copy():
+            for letter in gameState.grey_list:
                 if letter in word:
-                    word_list.remove(word)
+                    gameState.word_list.remove(word)
                     break
 
     @staticmethod
-    def keep_yellow_words(word_list, yellow_list):
-        for word in word_list.copy():
-            for letter in yellow_list:
+    def keep_yellow_words(): # take the list of currently valid words and removes all
+        for word in gameState.word_list.copy():
+            for letter in gameState.yellow_list:
                 if letter not in word:
-                    word_list.remove(word)
+                    gameState.word_list.remove(word)
                     break
 
     @staticmethod
-    def keep_green_words(word_list, green_list, index_list):
-        for word in word_list.copy():
-            for letter, index in zip(green_list, index_list):
+    def keep_green_words():
+        for word in gameState.word_list.copy():
+            for letter, index in zip(gameState.green_list, gameState.index_list):
                 if index < len(word) and word[index] != letter:
-                    word_list.remove(word)
+                    gameState.word_list.remove(word)
                     break
 
     @staticmethod
