efc443519ae41c99784b8b2309543847092202d7
PaulNusser <126136168+PaulNusser@users.noreply.github.com>
2024-04-18 18:13:09 -0700

    Leave File Io function empty for Nate to Implement Game.py

--- Game.py
+++ Game.py
@@ -7,13 +7,7 @@ class Game:
 
     def fillSet(filePath):
 
-        with open(filePath, 'r') as file:
-            mySet = set()
-            for line in file:
-                words = line.split()  # Split the line into words
-                mySet.update(words)  # Add the words to the set
-        return mySet
-    
+      
     def setHiddenWord(wordSet):
         return random.choice(tuple(wordSet))
     
