2261d98ef006ef5d06ed45f8bf5e08be1e5883b3
PaulNusser <pauli.nusser@gmail.com>
2024-04-19 18:14:10 -0700

    this versiom of game.py can be played by a user, the word is set in the code (no file/IO implementation yet)

--- Game.py
+++ Game.py
@@ -1,14 +1,12 @@
 import random
+import sys
 
 class Game:
     def __init__(self):
         self.wordList = []
         self.filePath = ""
-        self.secretWord = ""
-        self.wordFound = False
-        self.attempts = 0
-        self.userGuess = ""
         self.wordList = []
+        self.gamestate = gamestate()
     
     def fillwordList(self):
         newWordList = []
@@ -21,52 +19,98 @@ class Game:
         random_index = random.randint(0, (wordList.length()) - 1)
         return wordList[random_index]
     
-    def checkWord(guess,self):
+    def checkWord(self,guess):
         numList = []
-        found = False
+        
         for i in range(5):
+            found = False;
             for j in range(5):
-                if (self.secretWord[j] == guess[i]):
+                if (self.gamestate.secretWord[j] == guess[i]):
                     if (j == i):
-                        numList.append[2]
+                        numList.append(2)
                         found = True
                         break
                     else:
-                        numList.append[1]
+                        numList.append(1)
                         found = True
                         break
             if (found == False):
-                numList.append[0]
+                numList.append(0)
+        self.gamestate.updateGuesses(guess, numList)
         return numList
 
 
     
     def playGame(self):
-        self.filePath = input("enter the filePath of the list of all words: ")
-        wordSet = Game.fillSet(self.filePath)
-        self.wordList = Game.setToList(wordSet)
-        self.secretWord = Game.setHiddenWord(self.wordList)
+        ##self.filePath = input("enter the filePath of the list of all words: ")
+        ##wordSet = Game.fillSet(self.filePath)
+        ##self.wordList = Game.setToList(wordSet)
+        ##self.secretWord = Game.setHiddenWord(self.wordList)
         valGuess = False
-
-        while (self.wordFound == False & self.attempts < 6) :
+        self.gamestate.secretWord = "yerba"
+        
+        
+        while (self.gamestate.wordFound == False) :
+            valGuess = False
+            if (self.gamestate.attempts > 5):
+                print("you failed the word was: ")
+                print(self.gamestate.secretWord)
+                sys.exit()
+            
             while (valGuess == False) :
                 self.userGuess = input("make a guess: ") 
+                print()
                 if (len(self.userGuess) == 5):
                     valGuess = True
+                else :
+                    print("enter a 5 letter word")
             
-            
+            tempList = self.checkWord(self.userGuess)
+            print("Results for attempt #", self.gamestate.attempts, ", you have", 6 - self.gamestate.attempts, "attempts left")
+            print()
+            if (self.gamestate.attempts == 0):
+                print("resulting colors: (0 is grey, 1 is yellow and 2 is green) : ")
+                print()
+            print(self.gamestate.guesses[self.gamestate.attempts])
+            print()
+            if all(color == 2 for color in tempList):
+                self.gamestate.wordFound = True
+                print("you guessed it the secret word was: ")
+                print()
+                print(self.gamestate.secretWord)
+
+            else :
+                self.gamestate.attempts += 1
+        '''
+        print("Secret word is: {}".format(self.gamestate.secretWord))
+
+        guess = "dream"
+        self.checkWord(guess)
+        print(game1.gamestate.guesses)
+        self.checkWord("chart")
+        print(game1.gamestate.guesses)
+        self.checkWord("ranch")
+        print(game1.gamestate.guesses)
+        '''
 
 
 
+
             
-        
+       
 
 class gamestate:
 
     def __init__(self) -> None:
         self.guesses = []
-
+        self.secretWord = ""
+        self.wordFound = False
+        self.attempts = 0
+        self.userGuess = ""
 
     
-    def updateGuesses(self, word):
-        self.guesses.append(word)
+    def updateGuesses(self,guess, numList):
+        self.guesses.append(list(zip(guess, numList)))
+
+game1 = Game()
+game1.playGame()
