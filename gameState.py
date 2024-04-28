049bc6cce74154d091d32aebb09c361f18cc8cee
katherine-nemits <126533815+katherine-nemits@users.noreply.github.com>
2024-04-28 12:30:36 -0700

    Update gameState.py
    Comments added & edited for best practices

--- gameState.py
+++ gameState.py
@@ -1,37 +1,59 @@
+"""
+Manages state and logic of the Wordle Game
+"""
 class gameState:
 
     def __init__(self) -> None:
-        self.guesses = []
-        self.secretWord = ""
-        self.wordFound = False
-        self.attempts = 1
-        self.userGuess = ""
-        self.validGuesses = []
-        self.validSolutions = []
-        self.invalidChars = []
+        """
+        Initializes the game state with default values.
+        """
+        self.guesses = []   # List to store guessed words and their feedback
+        self.secretWord = ""    # secret word to be guessed
+        self.wordFound = False  # true if secret word is found
+        self.attempts = 1   # counter for number of guesses used
+        self.userGuess = "" # current guess
+        self.validGuesses = []  # List of valid guesses
+        self.validSolutions = []  # List of valid solutions
+        self.invalidChars = []  # invalid characters
 
 
     
     def updateGuesses(self, guess, numList):
+        """
+        Updates list of guesses with latest guess and feedback.
+
+        Args:
+            guess: guessed word
+            numList: List with feedback from guessed word
+        """
         self.guesses.append(list(zip(guess, numList)))
 
     def checkWord2(self,guess):
-        numList = []
+        """
+        Checks to see if guessed word is secret word and generates feedback
+
+        Args:
+            guess: guessed word
+
+        Returns:
+            List with feedback for guessed word
+        """
+        numList = []  # feedback list
         
         for i in range(5):
             found = False;
             for j in range(5):
                 if (self.secretWord[j] == guess[i]):
                     if (j == i):
-                        numList.append(2)
+                        numList.append(2)  # Correct letter, correct position - Green ('2')
                         found = True
                         break
                     else:
-                        numList.append(1)
+                        numList.append(1)  # Correct letter, wrong position - Yellow ('1')
                         found = True
                         break
             if (found == False):
-                numList.append(0)
+                numList.append(0)  # Incorrect letter - Grey ('0')
         return numList
     
 
